0f781257489760ac81bc2d0470b46555ab44dc6e
int.regio.li <li1160191297@qq.com>
2021-11-02 17:42:41 +0800

    1

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
 import cv2
 import os
 import numpy as np
-
-
-def video2frame(video_path, frame_path):
-    capture = cv2.VideoCapture(video_path)
-    flag, frame = capture.read()
-    i = 0
-    while flag:
-        flag, frame = capture.read()
-        fileName = "image%d.jpg" % i
-        i += 1
-        res = cv2.imwrite(os.path.join(frame_path, fileName), frame)
-
-
-def frame2video(frame_path, video_path, fps):
-    frame_list = os.listdir(frame_path)
-    img = cv2.imread(frame_path+frame_list[0])
-    print(img.shape[:2])
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    videoWriter = cv2.VideoWriter(video_path, fourcc, fps,frameSize = img.shape[:2])
-    for frame in frame_list:
-        frame_name = os.path.join(frame_path, frame)
-        img = cv2.imread(frame_name)
-        # cv2.imshow('image',img)
-        # cv2.waitKey(0)
-        frame = cv2.imread(np.fromfile(frame_name, dtype=np.uint8), -1)
-        videoWriter.write(img)
-    videoWriter.release()
-
+import videoUtil
 
 if __name__ == '__main__':
-    frame_path = 'evaluations/vid4/TMNet/walk/'  # 帧存放路径
-    video_path = 'test.mp4'  # 合成视频存放的路径
-    fps = 30  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
-    frame2video(frame_path, video_path, fps)
+    frame_path = 'datasets/LR/'  # 帧存放路径
+    video_path = 'result/1.avi'  # 合成视频存放的路径
+    fps = 50  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
+    videoUtil.frame2video(frame_path, video_path, fps)
+
+    # video_path = 'v_ApplyEyeMakeup_g05_c01.avi'
+    # frame_path = './video2frame'
+    # videoUtil.video2frame(video_path,frame_path)
---
+++ videoUtil.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import numpy as np
+
+
+def video2frame(video_path, frame_path):
+    capture = cv2.VideoCapture(video_path)
+    flag, frame = capture.read()
+    i = 0
+    while flag:
+        flag, frame = capture.read()
+        if not flag:
+            break
+        fileName = "image%d.jpg" % i
+        i += 1
+        print(os.path.join(frame_path, fileName))
+        res = cv2.imwrite(os.path.join(frame_path, fileName), frame)
+
+
+def frame2video(frame_path, video_path, fps):
+    frame_list = os.listdir(frame_path)
+    img = cv2.imread(frame_path + frame_list[0])
+    print(img.shape[:2])
+    frameSize = img.shape[:2][::-1]
+    print('img size' + str(frameSize))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    '''frameSize和img shape的关系'''
+    videoWriter = cv2.VideoWriter(video_path, fourcc, fps, frameSize=frameSize)
+    for i, frame in enumerate(frame_list):
+        print("processing: %d/%d" % (i + 1, len(frame_list)))
+        frame_name = os.path.join(frame_path, frame)
+        img = cv2.imread(frame_name)
+
+        videoWriter.write(img)
+    videoWriter.release()
+
+
+if __name__ == '__main__':
+    frame_path = 'datasets/evaluations/vid4/TMNet/walk/'  # 帧存放路径
+    video_path = 'result/test.avi'  # 合成视频存放的路径
+    fps = 30  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
+    frame2video(frame_path, video_path, fps)

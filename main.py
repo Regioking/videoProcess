48a81d1ff07246d8c995f7ca3906ef39f5703a74
int.regio.li <li1160191297@qq.com>
2021-11-04 14:51:20 +0800

    1

--- main.py
+++ main.py
@@ -3,12 +3,30 @@ import os
 import numpy as np
 import videoUtil
 
-if __name__ == '__main__':
-    frame_path = 'datasets/LR/'  # 帧存放路径
-    video_path = 'result/1.avi'  # 合成视频存放的路径
-    fps = 50  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
-    videoUtil.frame2video(frame_path, video_path, fps)
 
-    # video_path = 'v_ApplyEyeMakeup_g05_c01.avi'
-    # frame_path = './video2frame'
-    # videoUtil.video2frame(video_path,frame_path)
+def f2v():
+    src_frame_dir = './src_frame'
+    gen_video_dir = './gen_video'
+    for frame_set in os.listdir(src_frame_dir):
+        frame_path = os.path.join(src_frame_dir, frame_set)  # 帧存放路径
+        video_path = os.path.join(gen_video_dir, frame_set + str('.avi'))  # 合成视频存放的路径
+        fps = 50  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
+        videoUtil.frame2video(frame_path, video_path, fps)
+
+
+def v2f():
+    video_dir = './src_video/'
+    frame_dir = './gen_frame'
+    video_path_list = []
+    for video_name in os.listdir(video_dir):
+        if os.path.splitext(video_name)[1] == '.avi':
+            video_path = os.path.join(video_dir, video_name)
+            frame_path = os.path.join(frame_dir, os.path.splitext(video_name)[0])
+            if not os.path.exists(frame_path):
+                os.makedirs(frame_path)
+            print(frame_path)
+            videoUtil.video2frame(video_path, frame_path)
+
+
+if __name__ == '__main__':
+    v2f()
--- videoUtil.py
+++ videoUtil.py
@@ -37,6 +37,6 @@ def frame2video(frame_path, video_path, fps):
 
 if __name__ == '__main__':
     frame_path = 'datasets/evaluations/vid4/TMNet/walk/'  # 帧存放路径
-    video_path = 'result/test.avi'  # 合成视频存放的路径
+    video_path = 'src_video/test.avi'  # 合成视频存放的路径
     fps = 30  # 帧率，每秒钟帧数越多，所显示的动作就会越流畅
     frame2video(frame_path, video_path, fps)
